197e320a88f54caf08739bd3b6d2da3f18d97e01
asdfgsdsd <61223744+asdfgsdsd@users.noreply.github.com>
2020-02-19 00:19:20 -0800

    Add downloader script for model

---
+++ getModel.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+curl -LO https://github.com/mozilla/DeepSpeech/releases/download/v0.6.1/deepspeech-0.6.1-models.tar.gz
+tar xvf deepspeech-0.6.1-models.tar.gz
+rm deepspeech-0.6.1-models.tar.gz
+rm ._deepspeech-0.6.1-models
